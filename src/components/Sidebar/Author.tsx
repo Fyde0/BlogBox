e9174922f7f88eb9cab9b3aea9e6c92572b359b1
Federico <fyde90@gmail.com>
2024-08-09 14:51:14 +0200

    better author card maybe

--- src/components/Sidebar/Author.tsx
+++ src/components/Sidebar/Author.tsx
@@ -7,16 +7,15 @@ function Author({ userInfo }: { userInfo: IUserInfo }) {
         <div>
             <h5>Author</h5>
             <Card>
-                <Card.Body>
-                    <div>
+                <Card.Body className="d-flex flex-column gap-2">
+                    <div className="d-flex justify-content-around align-items-center">
+                        <h5 className="">{userInfo.name || userInfo.username}</h5>
                         <Avatar
                             avatar={userInfo.avatar}
-                            size={96}
-                            className="float-start me-2"
+                            size={64}
                         />
-                        <h5>{userInfo.name || userInfo.username}</h5>
-                        {userInfo.about}
                     </div>
+                    {userInfo.about}
                 </Card.Body>
             </Card>
         </div>
